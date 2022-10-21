17e7b7c203fa7e36e48bc5474891f476286d107d
André F S <58055547+andreflores2009@users.noreply.github.com>
2022-10-21 19:25:12 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.titulo("Aplicativo de IA")
+st.title("Aplicativo de IA")
 nome = st.text_input("Digite o nome")
 
 st.write("Bem vindo", nome, "ao seu primeiro aplicativo")
