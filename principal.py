1dc0d52a26a05447f663a214ba8d175702894bc7
André F S <58055547+andreflores2009@users.noreply.github.com>
2022-10-21 19:34:48 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Aplicativo de IA")
 nome = st.text_input("Digite o nome")
 if st.button("Clique aqui"):
-st.write("Bem vindo", nome, "ao seu primeiro aplicativo")
+  st.write("Bem vindo", nome, "ao seu primeiro aplicativo")
